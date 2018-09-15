eb2f68ebaa03143552fbdd5beacd3b790c31f25e
vultures <t.moll@outlook.com>
2018-09-15 17:36:59 -0400

    Calibrated ice cream rotation and position

--- threedee.js
+++ threedee.js
@@ -2,13 +2,15 @@ var canvas;
 var cone;
 
 function preload() {
-	cone = loadModel("images/iceCream2.obj");
-	
+	cone = loadModel("images/iceCreamCone.obj");
+	cream = loadModel("images/iceCream.obj");
 }
 
 function setup() {
-	createCanvas(windowWidth,300,WEBGL);
-	canvas.globalAlpha = 0.4;
+	canvas = createCanvas(windowWidth,windowHeight/2,WEBGL);
+	canvas.position(0,0);
+	canvas.style('z-index','-1');
+	angleMode(DEGREES);
 }
 
 function draw() {
@@ -17,13 +19,16 @@ function draw() {
 	directionalLight(255,255,255,0,0,1);
 
 	
-	//scale(10.0);
-	//model(cone);
-	//box(80);
-	rotateX(0.30);
-	rotateY(0);
-	rotateZ(frameCount * 0.01);
+	scale(1.5);
+	rotateX(0);
+	rotateY(frameCount * 0.2);
+	rotateZ(180);
 	normalMaterial();
-	translate(0,100,0);
+	translate(0,0,0);
 	model(cone);
+	model(cream);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight/2);
 }
