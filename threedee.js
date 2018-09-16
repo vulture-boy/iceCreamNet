2849be74bc5809f2621fb55e0e3f5a2e3592be32
vultures <t.moll@outlook.com>
2018-09-16 18:51:33 -0400

    Texture and Video Capture sphere

--- threedee.js
+++ threedee.js
@@ -4,6 +4,9 @@ var cone;
 function preload() {
 	cone = loadModel("images/iceCreamCone.obj");
 	cream = loadModel("images/iceCream.obj");
+	creamTex = loadImage("images/cream3.png");
+	cam = createCapture(VIDEO);
+	cam.hide();
 }
 
 function setup() {
@@ -19,17 +22,28 @@ function windowResized() {
 
 function draw() {
 	background("white");
-	ambientLight(105);
+	ambientLight(233);
 	directionalLight(255,255,255,0,0,1);
-
-	
-	scale(3);
-	rotateX(0);
-	rotateY(frameCount * 0.2);
-	rotateZ(180);
-	normalMaterial();
-	translate(0,0,0);
-	model(cone);
-	model(cream);
+	var mousePos = mouseY*-1
+	// Ice Cream Cone
+	push();
+		scale(3);
+		rotateX(0 + mouseY/8);
+		rotateY(frameCount * 0.2);
+		rotateZ(180 + mouseY/8);
+		normalMaterial();
+		model(cone);
+		texture(creamTex);
+		model(cream);
+	pop();
+	translate(0,-300,-800 + mouseY);
+	// You
+	push();
+		rotateY(180);
+		scale(2);
+		tex = texture(cam);
+		tex.scale(-1,1);
+		sphere(150);
+	pop();
 }
 
