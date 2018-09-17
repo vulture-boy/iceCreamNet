122565341cbfa8d677777a62a50dff1e67b2d035
vultures <t.moll@outlook.com>
2018-09-16 22:48:40 -0400

    Improved UI, tipping. Abandoned "about" page.

--- threedee.js
+++ threedee.js
@@ -4,10 +4,14 @@ var cone;
 function preload() {
 	cone = loadModel("images/iceCreamCone.obj");
 	cream = loadModel("images/iceCream.obj");
-	flavourA = loadImage("images/cream1.png");
-	flavourB = loadImage("images/cream2.png");
-	flavourC = loadImage("images/cream3.png");
+	flavourA = loadImage("images/cream1.png"); // Vanilla
+	flavourB = loadImage("images/cream2.png"); // Chocolate
+	flavourC = loadImage("images/cream3.png"); // Strawberry
+	noA = loadImage("images/num1.png");
+	noB = loadImage("images/num2.png");
+	noC = loadImage("images/num3.png");
 	flavPalette = loadImage("images/flavours.png");
+	coneTex = loadImage("images/cone.png");
 	cam = createCapture(VIDEO);
 	cam.hide();
 }
@@ -17,7 +21,11 @@ function setup() {
 	canvas.position(0,0);
 	canvas.style('z-index','-1');
 	angleMode(DEGREES);
-	selFlavour = 1;
+	selFlavour = 2;
+	selCone = 1;
+	tipToggle = 0;
+	tipVal = 0;
+	noCursor();
 }
 
 function windowResized() {
@@ -25,23 +33,22 @@ function windowResized() {
 }
 
 function draw() {
-	loadPixels();
 	background('rgba(0,0,0,0)');
 	orbitControl();
 	ambientLight(233);
 	directionalLight(255,255,255,0,0,1);
-	var mousePos = mouseY*-1;
-	
+	if (tipToggle != 0)	{tipVal = mouseX*0.3;}
 	// Ice Cream Cone
 	push();
 		scale(3);
 		rotateX(0);
 		rotateY(frameCount * 0.2);
-		rotateZ(180);
-		normalMaterial();
+		rotateZ(180 - tipVal);
+		if (selCone == 2) {normalMaterial();}
+		else if (selCone == 1) {texture(coneTex);}
 		model(cone);
-		if (selFlavour == 1) {texture(flavourA);}
-		else if (selFlavour == 2) {texture(flavourB);}
+		if (selFlavour == 2) {texture(flavourA);}
+		else if (selFlavour == 1) {texture(flavourB);}
 		else if (selFlavour == 3) {texture(flavourC);}
 		model(cream);
 	pop();
@@ -67,23 +74,51 @@ function draw() {
 		// div by 2: planes draw from centre outwards
 		translate((windowWidth-palWidth -palPaddingX )/2,(windowHeight-palHeight -palPaddingY)/2,10);
 		plane(palWidth, palHeight);
-		translate(0,flavHeight/2,1);
+		translate(0,flavHeight/2 - 6,1); // Flavour 1
 		texture(flavourA);
 		plane(flavWidth, flavHeight);
-		translate(-1* palWidth/2 + flavWidth,0,0);
+		translate(0,0,1); // Num 2
+		texture(noB);
+		plane(noB.width/2,noB.height/2);
+		translate(-1* palWidth/2 + flavWidth,0,-1); // Flavour 2
 		texture(flavourB);
 		plane(flavWidth, flavHeight);
-		translate(palWidth - flavWidth*2,0,0);
+		translate(0,0,1); // Num 1
+		texture(noA);
+		plane(noA.width/2,noA.height/2);
+		translate(palWidth - flavWidth*2,0,-1); // Flavour 3
 		texture(flavourC);
 		plane(flavWidth, flavHeight);
+		translate(0,0,1); // Num 3
+		texture(noC);
+		plane(noC.width/2,noC.height/2);
 	pop();
-	
+}
+
+function keyPressed() {
+	// Flavour selection
+	if (key == '1') {selFlavour = 1;} // Chocolate
+	else if (key == '2') {selFlavour = 2;} // Vanilla
+	else if (key == '3') {selFlavour = 3;} // Strawberry
+	else if (key == '9') {selCone = 1;} // Normals Cone
+	else if (key == '0') {selCone = 2;} // Authentic Cone
+	else if (key == 'q') // Enable / Disable Cone Tipping Movement
+		{
+			if (tipToggle == 1) {tipToggle = 0;}
+			else {tipToggle = 1;}
+		}
+}
+
+function mousePressed() {
+	// Select flavour via pixel colour
+	// NOTE: doesn't load the gpu display properly in WEBGL mode...
+	//		without some major digging, probably wont work
+	loadPixels();
 	var pixel = [0,0,0];
 	pixel[0] = pixels[4*mouseX + 4*windowWidth*mouseY]
 	pixel[1] = pixels[4*mouseX + 4*windowWidth*mouseY +1]
 	pixel[2] = pixels[4*mouseX + 4*windowWidth*mouseY +2]
-
-	// Select flavour via pixel colour
+	
 	if (mouseIsPressed) {
 		if (pixel == [255,116,166]) {
 			selFlavour = 3;
@@ -94,6 +129,7 @@ function draw() {
 		}
 		console.log(pixel[0]);
 	}
+	
 }
 
 
